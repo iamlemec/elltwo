001eb0d62029ccb76f36a34900ea3ae8990c396f
Douglas Hanley <thesecretaryofwar@gmail.com>
2020-06-26 22:57:07 -0400

    full editing functionality

--- db_populate.py
+++ db_populate.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -7,13 +8,15 @@ engine = create_engine('sqlite:///axiom.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-art = Article(aid=0, title='Abraham Lincoln')
-par0 = Paragraph(pid=0, aid=0, text='# Abraham Lincoln')
-par1 = Paragraph(pid=1, aid=0, text='He was a dude.')
-par2 = Paragraph(pid=2, aid=0, text='He got shot.')
-lin0 = Paralink(aid=0, pid=0, next=1)
-lin1 = Paralink(aid=0, pid=1, prev=0, next=2)
-lin2 = Paralink(aid=0, pid=2, prev=1)
+now = datetime.utcnow()
+
+art = Article(aid=0, title='Abraham Lincoln', create_time=now)
+par0 = Paragraph(pid=0, aid=0, text='# Abraham Lincoln', create_time=now)
+par1 = Paragraph(pid=1, aid=0, text='He was a dude.', create_time=now)
+par2 = Paragraph(pid=2, aid=0, text='He got shot.', create_time=now)
+lin0 = Paralink(aid=0, pid=0, next=1, create_time=now)
+lin1 = Paralink(aid=0, pid=1, prev=0, next=2, create_time=now)
+lin2 = Paralink(aid=0, pid=2, prev=1, create_time=now)
 
 session.add(art)
 session.add_all([par0, par1, par2])
--- db_query.py
+++ db_query.py
@@ -9,6 +9,33 @@ engine = create_engine('sqlite:///axiom.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+##
+## diagnostic tools
+##
+
+def getall(klass, disp=True):
+    ret = session.query(klass).all()
+    if disp:
+        print('\n'.join([str(x) for x in ret]))
+    else:
+        return ret
+
+def revert(time, klass=None):
+    if klass is None:
+        revert(time, klass=Article)
+        revert(time, klass=Paragraph)
+        revert(time, klass=Paralink)
+    else:
+        for k in session.query(klass).filter(klass.create_time > time).all():
+            session.delete(k)
+        for k in session.query(klass).filter(klass.delete_time > time).all():
+            k.delete_time = None
+        session.commit()
+
+##
+## utility methods
+##
+
 def intime(time, klass):
     return and_(
         klass.create_time <= time,
@@ -21,13 +48,6 @@ arttime = partial(intime, klass=Article)
 partime = partial(intime, klass=Paragraph)
 lintime = partial(intime, klass=Paralink)
 
-def getall(klass, disp=True):
-    ret = session.query(klass).all()
-    if disp:
-        print('\n'.join([str(x) for x in ret]))
-    else:
-        return ret
-
 def find_start(links):
     for p in links:
         if p.prev is None:
@@ -38,28 +58,36 @@ def link_sort(links):
         return
     else:
         yield p.pid
-    index = {p.pid: p for p in links}
-    while (p := index.get(p.next, None)) is not None:
+    index = {q.pid: q for q in links if q is not p}
+    while (p := index.pop(p.next, None)) is not None:
         yield p.pid
+
+def splice_link(lin, time, **kwargs):
+    props = {**dict(pid=lin.pid, prev=lin.prev, next=lin.next), **kwargs}
+    lin.delete_time = time
+    return Paralink(aid=lin.aid, create_time=time, **props)
+
+##
+## query methods
+##
 
 def get_links(aid, time=None):
     if time is None:
         time = datetime.utcnow()
-
     query = (session
         .query(Paralink)
         .filter_by(aid=aid)
         .filter(lintime(time))
     )
-    links = query.all()
+    return query.all()
 
+def get_pids(aid, time=None):
+    links = get_links(aid, time=time)
     return list(link_sort(links))
 
 def get_paras(aid, pids=None, time=None):
-    if time is None:
-        time = datetime.utcnow()
     if pids is None:
-        pids = get_links(aid, time=time)
+        pids = get_pids(aid, time=time)
 
     query = (session
         .query(Paragraph)
@@ -83,6 +111,10 @@ def get_link(pid, time=None):
         time = datetime.utcnow()
     return session.query(Paralink).filter_by(pid=pid).filter(lintime(time)).one_or_none()
 
+##
+## editing methods
+##
+
 def update_para(pid, text):
     now = datetime.utcnow()
 
@@ -94,51 +126,94 @@ def update_para(pid, text):
     linp = get_link(lin.prev, now)
     linn = get_link(lin.next, now)
 
+    par.delete_time = now
     par1 = Paragraph(aid=par.aid, create_time=now, text=text)
     session.add(par1)
     session.commit()
 
-    par.delete_time = now
-    lin.delete_time = now
-    lin1 = Paralink(aid=par.aid, pid=par1.pid, prev=lin.prev, next=lin.next, create_time=now)
+    lin1 = splice_link(lin, now, pid=par1.pid)
     session.add(lin1)
 
     if linp is not None:
-        linp.delete_time = now
-        linp1 = Paralink(aid=par.aid, pid=linp.pid, prev=linp.prev, next=par1.pid, create_time=now)
+        linp1 = splice_link(linp, now, next=par1.pid)
         session.add(linp1)
     if linn is not None:
-        linn.delete_time = now
-        linn1 = Paralink(aid=par.aid, pid=linn.pid, prev=par1.pid, next=linn.next, create_time=now)
+        linn1 = splice_link(linn, now, prev=par1.pid)
         session.add(linn1)
 
     session.commit()
 
-def insert_after(aid, pid, text):
+def insert_after(pid, text):
     now = datetime.utcnow()
 
+    if (par := get_para(pid)) is None:
+        return
     if (lin := get_link(pid, now)) is None:
         return
 
     linn = get_link(lin.next, now)
 
-    par1 = Paragraph(aid=aid, text=text, create_time=now)
+    par1 = Paragraph(aid=par.aid, text=text, create_time=now)
     session.add(par1)
     session.commit()
 
-    lin.delete_time = now
-    lin1 = Paralink(aid=aid, pid=pid, prev=lin.prev, next=par1.pid, create_time=now)
-    lin2 = Paralink(aid=aid, pid=par1.pid, prev=pid, next=lin.next, create_time=now)
-    session.add_all([lin1, lin2])
+    lin0 = Paralink(aid=par.aid, pid=par1.pid, prev=pid, next=lin.next, create_time=now)
+    session.add(lin0)
+
+    lin1 = splice_link(lin, now, next=par1.pid)
+    session.add(lin1)
 
     if linn is not None:
-        linn.delete_time = now
-        linn1 = Paralink(aid=aid, pid=lin.next, prev=par1.pid, next=linn.next, create_time=now)
+        linn1 = splice_link(linn, now, prev=par1.pid)
         session.add(linn1)
 
     session.commit()
 
-if __name__ == '__main__':
-    for art in getall(Article):
-        print(f'{art.aid}: {art.title}')
-        print(get_text(art.aid))
+def insert_before(pid, text):
+    now = datetime.utcnow()
+
+    if (par := get_para(pid)) is None:
+        return
+    if (lin := get_link(pid, now)) is None:
+        return
+
+    linp = get_link(lin.prev, now)
+
+    par1 = Paragraph(aid=par.aid, text=text, create_time=now)
+    session.add(par1)
+    session.commit()
+
+    lin0 = Paralink(aid=par.aid, pid=par1.pid, prev=lin.prev, next=pid, create_time=now)
+    session.add(lin0)
+
+    lin1 = splice_link(lin, now, prev=par1.pid)
+    session.add(lin1)
+
+    if linp is not None:
+        linp1 = splice_link(linp, now, next=par1.pid)
+        session.add(linp1)
+
+    session.commit()
+
+def delete_para(pid):
+    now = datetime.utcnow()
+
+    if (par := get_para(pid)) is None:
+        return
+    if (lin := get_link(pid, now)) is None:
+        return
+
+    linp = get_link(lin.prev, now)
+    linn = get_link(lin.next, now)
+
+    par.delete_time = now
+    lin.delete_time = now
+
+    if linp is not None:
+        linp1 = splice_link(linp, now, next=lin.next)
+        session.add(linp1)
+    if linn is not None:
+        linn1 = splice_link(linn, now, prev=lin.prev)
+        session.add(linn1)
+
+    session.commit()
