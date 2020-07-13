b97c1bcdc44b5027fe386584d8ce705c5ee1cdc4
Evan Piermont <evan.piermont@gmail.com>
2020-07-12 23:27:50 -0400

    js local updates

--- static/js/render.js
+++ static/js/render.js
@@ -2,20 +2,55 @@
 recvCommand = function(cmd, data) {
     msg = JSON.stringify(data);
     console.log("received [" + cmd + "]: " + msg);
+    if(cmd=='updatePara'){
+        updatePara(...data); //data must come in correct order or args
+    }else if(cmd=='deletePara'){
+        deletePara(...data) 
+    }else if(cmd=='insert'){
+        insert(...data) 
+    }else{
+        console.log('unknown cmd')
+    }
 }
 client = Client(recvCommand);
 
 // get raw text from data-raw attribute, parse, render
-dataToText = function() {
-    $('.para').each(function() {
-        raw = $(this).data('raw');
+dataToText = function(para, raw="") {
+    if (!raw){
+        raw = para.data('raw');
+    }
         html_text = markthree(raw);
-        $(this).html(html_text)
-    });
+        para.html(html_text);
  };
 
 $(document).ready(function() {
     var url = "http://" + document.domain + ':' + location.port;
     client.connect(url);
-    dataToText();
+    $('.para').each(function() {
+        dataToText($(this));
+    });
 });
+
+/// editing commands for paras (triggered by incoming socket commands)
+/// local changes only --> to reflect server changes without full reload
+
+updatePara = function(pid, raw){
+    para = $('#para_' + pid);
+    para.data('raw', raw);
+    dataToText(para, raw);
+};
+
+deletePara = function(pid){
+    para = $('#para_' + pid);
+    para.remove()
+};
+
+insert = function(pid, new_pid, before=true){
+    para = $('#para_' + pid);
+    new_para = `<div class="para" id=para_`+new_pid+` data-raw="">...</div>`;
+    if(before){
+        para.before(new_para);
+    }else{
+        para.after(new_para);
+    };
+};
