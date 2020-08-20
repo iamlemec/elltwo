faba689bc616bdef3f05861e473af0766384b9be
Douglas Hanley <thesecretaryofwar@gmail.com>
2020-08-20 13:34:50 -0400

    fix db populate bug

--- db_populate.py
+++ db_populate.py
@@ -19,12 +19,12 @@ lin0 = Paralink(aid=0, pid=0, next=1, create_time=now)
 lin1 = Paralink(aid=0, pid=1, prev=0, next=2, create_time=now)
 lin2 = Paralink(aid=0, pid=2, prev=1, create_time=now)
 
-for fname in os.listdir('testing'):
-    title, _ = os.path.splitext(fname)
-    mark = open(f'testing/{fname}').read()
-    import_markdown(title, mark)
-
 db.session.add(art)
 db.session.add_all([par0, par1, par2])
 db.session.add_all([lin0, lin1, lin2])
 db.session.commit()
+
+for fname in os.listdir('testing'):
+    title, _ = os.path.splitext(fname)
+    mark = open(f'testing/{fname}').read()
+    import_markdown(title, mark)
