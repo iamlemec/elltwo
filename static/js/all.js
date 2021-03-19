ad882f4b7ff8bcfffeebfb88d4e2744ba6130f9d
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-03-19 00:21:45 -0400

    button typo

--- static/js/all.js
+++ static/js/all.js
@@ -20,7 +20,7 @@ toggleBox = function(bool, button, box) {
             var targ = $(e.target);
             var close_butt = targ.closest('button');
             var close_box = targ.closest('box');
-            if ((close_butt.length == 0) && (close_box.lenght == 0)) {
+            if ((close_butt.length == 0) && (close_box.length == 0)) {
                 $(box).hide();
                 bool = false;
             }
