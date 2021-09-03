8e0c56f8f8a92fe730b512c781cf9a422eec8486
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-09-03 01:26:55 -0400

    demo check db exists

--- demo.sh
+++ demo.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
 
-python -u console --db=/opt/data/elltwo.db backup load testing
-python -u server.py --db=/opt/data/elltwo.db --ip=0.0.0.0 --theme=white --demo
+# global options
+E2DB="/opt/data/elltwo.db"
+ADDR="0.0.0.0"
+THEME="white"
+
+# create database if needed
+if [ ! -f "${E2DB}" ]; then
+    python -u console --db="${E2DB}" backup load testing
+fi
+
+# start server
+python -u server.py --db="${E2DB}" --ip="${ADDR}" --theme="${THEME}" --demo
