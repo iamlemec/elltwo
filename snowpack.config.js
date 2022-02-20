d7ad83fc2c22eee2046408e7c669307de7608e0c
Douglas Hanley <thesecretaryofwar@gmail.com>
2022-02-19 23:02:21 -0500

    snowpack css hack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,9 +1,16 @@
 export default {
     mount: {
         static: '/',
-    },
-    buildOptions: {
-        sourcemap: true,
+        "node_modules/gum.js/bin": {
+            url: "/css/gum",
+            static: true,
+            resolve: false,
+        },
+        "node_modules/katex/dist": {
+            url: "/css/katex",
+            static: true,
+            resolve: false,
+        }
     },
     devOptions: {
         open: 'none',
